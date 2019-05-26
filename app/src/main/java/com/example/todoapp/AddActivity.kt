4314d583df07fc8f700aae17088bcc9908e3c642
kab <kab3nan9@gmail.com>
2019-05-26 16:46:55 +0900

    DBにタスクを追加出来る様にした

--- app/src/main/java/com/example/todoapp/AddActivity.kt
+++ app/src/main/java/com/example/todoapp/AddActivity.kt
@@ -3,6 +3,8 @@ package com.example.todoapp
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import kotlinx.android.synthetic.main.activity_add.*
 
 class AddActivity : AppCompatActivity() {
@@ -10,6 +12,16 @@ class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        val tasksDatabaseHelper = TasksDatabaseHelper(this)
+
+        AddButton.setOnClickListener { view -> val task = TaskModel(Add_task.text.toString(), "0")
+            val result = tasksDatabaseHelper.insertTask(task)
+
+            if(result){
+                Toast.makeText(this,"new Task Added!", LENGTH_LONG).show()
+            }
+        }
 
         BackButton.setOnClickListener {
             val intent = Intent(application,MainActivity::class.java)
